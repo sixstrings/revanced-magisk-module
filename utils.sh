bb1a0b3a50bceef40eb6d9380089d04c2c2219d4
Wolfcity <132693162+sixstrings@users.noreply.github.com>
2025-09-23 15:59:55 +0200

    Update utils.sh
    Autor J-hc (forked by sixstrings)

--- utils.sh
+++ utils.sh
@@ -652,7 +652,7 @@ module_prop() {
 name=${2}
 version=v${3}
 versionCode=${NEXT_VER_CODE}
-author=j-hc & sixstrings
+author=j-hc (fork by sixstrings)
 description=${4}" >"${6}/module.prop"
 
 	if [ "$ENABLE_MAGISK_UPDATE" = true ]; then echo "updateJson=${5}" >>"${6}/module.prop"; fi
