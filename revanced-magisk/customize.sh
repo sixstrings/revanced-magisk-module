a650033369221388b438f3caa16f8889b6b8a353
Wolfcity <132693162+sixstrings@users.noreply.github.com>
2025-12-20 00:23:13 +0100

    Update attribution message in customize.sh

--- revanced-magisk/customize.sh
+++ revanced-magisk/customize.sh
@@ -176,4 +176,5 @@ nohup cmd package compile --reset "$PKG_NAME" >/dev/null 2>&1 &
 rm -rf "${MODPATH:?}/bin" "$MODPATH/$PKG_NAME.apk"
 
 ui_print "* Done"
-ui_print "  forked by sixstrings (github.com/sixstrings)"
+ui_print "  by sixstrings (github.com/sixstrings)"
+ui_print " "
