60519804c36229b06668c5ace9193c063ccc52c5
Wolfcity <132693162+sixstrings@users.noreply.github.com>
2026-01-17 19:14:42 +0100

    Update UI print messages in customize.sh

--- revanced-magisk/customize.sh
+++ revanced-magisk/customize.sh
@@ -175,6 +175,7 @@ nohup cmd package compile --reset "$PKG_NAME" >/dev/null 2>&1 &
 
 rm -rf "${MODPATH:?}/bin" "$MODPATH/$PKG_NAME.apk"
 
-ui_print "  by sixstrings"
-ui_print "  Done"
+ui_print "* Done"
+ui_print "  by sixstrings (github.com/sixstrings)"
+ui_print " "
 
